bb423d4f4f2b5a75b737c52455b589de48d7d142
chienpv <phamvanbk11@gmail.com>
2024-11-11 13:13:31 +0700

    config routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { LoginComponent } from './login/login.component';
+import { ProductsComponent } from './components/products/products.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { RootComponent } from './root/root.component';
+import { AdminComponent } from './admin/admin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: RootComponent,
+    children: [
+      { path: 'home', component: HomeComponent },
+      { path: 'about', component: AboutComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'products', component: ProductsComponent },
+      { path: '', redirectTo: '/home', pathMatch: 'full' },
+    ],
+  },
+
+  {
+    path: 'admin',
+    component: AdminComponent,
+    pathMatch: 'prefix',
+    children: [{ path: 'products', component: ProductsComponent }],
+  },
+
+  { path: '**', component: PageNotFoundComponent },
+];
