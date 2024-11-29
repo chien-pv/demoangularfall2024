e160d79bc9b710354d5b36296bb7435cb710500a
chienpv <phamvanbk11@gmail.com>
2024-11-29 12:59:23 +0700

    canactive

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,12 +13,28 @@ import { RootComponent } from './root/root.component';
 import { AdminComponent } from './admin/admin.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { UsersComponent } from './users/users.component';
+import { Router } from '@angular/router';
+import { jwtDecode } from 'jwt-decode';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 const canActivateLogin: CanActivateFn = (
   route: ActivatedRouteSnapshot,
   state: RouterStateSnapshot
 ) => {
-  return true;
+  let router: Router = new Router();
+  const token = localStorage.getItem('accsesToken');
+  const jwtHelper = new JwtHelperService();
+  try {
+    if (!jwtHelper.isTokenExpired(token)) {
+      const decoded: any = jwtDecode(token as string);
+      router.navigateByUrl('/home');
+      return false;
+    } else {
+      return true;
+    }
+  } catch (error) {
+    return true;
+  }
 };
 
 export const routes: Routes = [
@@ -27,12 +43,19 @@ export const routes: Routes = [
     component: RootComponent,
     children: [
       {
+        path: '',
+        component: HomeComponent,
+      },
+      {
         path: 'home',
-        canActivate: [canActivateLogin],
         component: HomeComponent,
       },
       { path: 'about', component: AboutComponent },
-      { path: 'login', component: LoginComponent },
+      {
+        path: 'login',
+        component: LoginComponent,
+        canActivate: [canActivateLogin],
+      },
       { path: 'products', component: ProductsComponent },
       { path: 'products/:id', component: ProductDetailComponent },
       { path: '', redirectTo: '/home', pathMatch: 'full' },
--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { jwtDecode } from 'jwt-decode';
-
+import { JwtHelperService } from '@auth0/angular-jwt';
 @Component({
   selector: 'app-header',
   standalone: true,
@@ -13,18 +13,30 @@ import { jwtDecode } from 'jwt-decode';
 export class HeaderComponent implements OnInit {
   firstName: string = 'Trần Văn';
   lastName: string = 'B';
-  isLogin = true;
+  isLogin = false;
   isSpecial = false;
   name = '';
   ngOnInit(): void {
     if (typeof localStorage !== 'undefined') {
       const token = localStorage.getItem('accsesToken');
-      const decoded: any = jwtDecode(token as string);
-      this.name = decoded.name;
+      const jwtHelper = new JwtHelperService();
+
+      try {
+        if (!jwtHelper.isTokenExpired(token)) {
+          const decoded: any = jwtDecode(token as string);
+          this.isLogin = true;
+          this.name = decoded.name;
+        } else {
+          this.isLogin = false;
+        }
+      } catch (error) {
+        this.isLogin = false;
+      }
     } else {
       console.log('Web Storage is not supported in this environment.');
     }
   }
+
   getName() {
     return this.firstName + ' ' + this.lastName;
   }
